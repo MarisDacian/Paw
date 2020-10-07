5e2dec7f02ca35a9846c4ac7cc65504d644a2bfc
Maris Dacian <dacian@xcommerce.eu>
2020-10-07 19:57:06 +0300

    t1

--- t1.php
+++ t1.php
@@ -1,4 +1,4 @@
-<form action="echoText.php" enctype="multipart/form-data" method="GET" > 
+<form action="https://your-app-name.herokuapp.com/echoText.php" enctype="multipart/form-data" method="POST" > 
     <input type="submit" name="join_queue" value="Join Queue">
 </form>
 
