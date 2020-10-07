1592419c6c0b946c405533bf1f9921b91b14aaa6
Maris Dacian <dacian@xcommerce.eu>
2020-10-07 20:15:32 +0300

    t1

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<form action="https://rocky-beyond-47360.herokuapp.com//echoText.php" enctype="multipart/form-data" method="POST" > 
+<form action="https://rocky-beyond-47360.herokuapp.com/echoText.php" enctype="multipart/form-data" method="POST" > 
     <input type="submit" name="join_queue" value="Join Queue">
 </form>
 
