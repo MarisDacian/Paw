67207d5d66b4dccd8943336aa7660a17531086b8
Maris Dacian <dacian@xcommerce.eu>
2020-10-07 19:55:28 +0300

    t1

--- t1.php
+++ t1.php
@@ -1,4 +1,4 @@
-<form action="echoText.php" enctype="multipart/form-data" method="POST" > 
+<form action="echoText.php" enctype="multipart/form-data" method="GET" > 
     <input type="submit" name="join_queue" value="Join Queue">
 </form>
 
